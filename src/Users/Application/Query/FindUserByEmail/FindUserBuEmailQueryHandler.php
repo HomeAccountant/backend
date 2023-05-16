16e4f98424d857c5c50f15b3dc2e66c32ebdc216
Mykyta Shelest <shelestmykyta2022@gmail.com>
2023-05-16 16:46:20 +0300

    implemented CQRS

---
+++ src/Users/Application/Query/FindUserByEmail/FindUserBuEmailQueryHandler.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Users\Application\Query\FindUserByEmail;
+
+use App\Shared\Application\Query\QueryHandlerInterface;
+use App\Users\Application\DTO\UserDTO;
+use App\Users\Domain\Repository\UserRepositoryInterface;
+
+class FindUserBuEmailQueryHandler implements QueryHandlerInterface
+{
+	public function __construct(private readonly UserRepositoryInterface $userRepository)
+	{
+	}
+
+	public function __invoke(FindUserByEmailQuery $query): ?UserDTO
+	{
+		$user = $this->userRepository->findByEmail($query->email);
+
+		if (!$user) {
+			return null;
+		}
+
+		return UserDTO::fromEntity($user);
+	}
+}
--- src/Users/Domain/Repository/UserRepositoryInterface.php
+++ src/Users/Domain/Repository/UserRepositoryInterface.php
@@ -2,7 +2,13 @@
 
 namespace App\Users\Domain\Repository;
 
+use App\Users\Domain\Entity\User;
+
 interface UserRepositoryInterface
 {
+	public function add(User $user): void;
 
+	public function findByUuid(string $uuid): User;
+
+	public function findByEmail(string $email): ?User;
 }
